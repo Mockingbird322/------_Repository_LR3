ba2c0a186909aa0e87720661f9c27d2011c17f4c
Mockingbird322 <sergey24020@gmail.com>
2025-05-22 21:27:12 +0300

    Добавили реализацию 2-ой функции

--- Knyazev_LR3_project.cpp
+++ Knyazev_LR3_project.cpp
@@ -58,8 +58,14 @@ void enterRealNumber() {
 }
 
 void enterNaturalNumber() {
-    // Реализация будет добавлена позже
-    cout << "[Функция ввода y]\n";
+     do {
+        cout << "Введите натуральное число y (целое > 0): ";
+        cin >> y;
+        if (y <= 0) {
+            cout << "Ошибка: введите положительное натуральное число.\n";
+        }
+    } while (y <= 0);
+    cout << "Сохранено: y = " << y << endl;
 }
 
 void roundX() {
